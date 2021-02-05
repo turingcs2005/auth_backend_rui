b5cddbd865749dce3b71cd58be8c7fa39c67cc96
Rui Wang <rui.wang.624@gmail.com>
2021-02-05 09:32:30 -0500

    auth back end by rui

--- routes/api.js
+++ routes/api.js
@@ -2,12 +2,18 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const User = require('../models/user');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 mongoose.connect(
-    "mongodb://localhost:27017/local",
+    process.env.DB_CONNECT,
     {
         useNewUrlParser: true,
-        useUnifiedTopology: true
+        useUnifiedTopology: true,
+        useFindAndModify: false
     },
 
     err => {
@@ -23,43 +29,89 @@ router.get('/', (req, res) => {
     res.send('API route');
 });
 
-router.post('/register', (req, res) => {
-    let user = new User(req.body);
-    user.save( (error, registeredUser) => {
-        if (error) {
-            console.error(error);
-        } else {
-            res.status(200).send(registeredUser.name);
-        }
-    } );
+// register a new account
+router.post('/register', async (req, res) => {
+
+    const u = await User.findOne({ email: req.body.email});
+
+    if (u) {     // if email already exist
+        res.status(400).json({error: 'Email exists.'});
+    } else {     
+        // hash password
+        const salt = await bcrypt.genSalt(10);
+        const hashPassword = await bcrypt.hash(req.body.password, salt);
+
+        // create a new user using hashed password
+        const user = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: hashPassword
+        });
+
+        user.save( (error, registeredUser) => {
+            if (error) {
+                console.error(error);
+            } else {
+                res.status(200).json(registeredUser);
+            }
+        });
+    }
 });
 
-router.post('/login', (req, res) => {
-    User.findOne({ email: req.body.email }, (error, user) => {
+// delete an existing account
+router.delete('/delete', (req, res) => {
+    User.findOneAndDelete( {email: req.body.email }, (error, user) => {
         if (error) {
             console.error(error);
         } else {
             if (!user) {
-                res.status(401).send('In valid email.');
+                res.status(400).json({error: 'Invalid email.'});
             } else {
-                if ( user.password !== req.body.password ) {
-                    res.status(401).send('Invalid password');
-                } else {
-                    res.status(200).send(user.name + ' has been logged in.');
-                }
+                res.status(200).json({message: 'Account has been deleted!'});
             }
         }
     });
 });
 
-router.get('/events', (req, res) => {
-    const events = ['happy hour', 'beer party'];
-    res.json(events);
+// update an existing account
+router.put('/update', (req, res) => {
+    User.findOneAndUpdate({ email: req.body.email }, req.body, (error, user) => {
+        if (error) {
+            console.error(error);
+        } else {
+            if (!user) {
+                res.status(400).json({error: 'Invalid email.'});
+            } else {
+                res.status(200).json({status: 'Account has been updated.'});
+            }
+        }
+    });
 });
 
-router.get('/special', (req, res) => {
-    const special = ['Disney World', 'Hawaii tour'];
-    res.json(special);
+// login
+router.post('/login', async (req, res) => {
+    const u = await User.findOne({email: req.body.email});
+    if (!u) {
+        res.status(400).json({error: 'Invalid email.'});
+    } else {
+        // check password
+        const validPass = await bcrypt.compare(req.body.password, u.password);
+        if (!validPass) {
+            res.status(400).json({error: 'Invalid password.'});
+        } else {
+            // create and sign a token
+            const token = jwt.sign({_id: u._id}, process.env.TOKEN_SECRET);
+            res.status(200).header('auth-token', token).send(token);
+        }
+    }
+});
+
+router.get('/free-access', (req, res) => {
+    res.status(200).json({response: 'No authentication.'})
+});
+
+router.get('/authorized-access', (req, res) => {
+    res.status(200).json({response: 'Authentication.'});
 })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const api = require('./routes/api');
 
+app.use(cors());
 app.use(express.json());
+
 app.use('/api', api);
 
 app.get('/', (req, res) => {
